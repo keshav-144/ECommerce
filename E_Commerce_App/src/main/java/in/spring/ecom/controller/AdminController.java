7e4dfa65f9797c5a88afa12274c2ab9249309712
Keshav Kumar <167737709+keshav-144@users.noreply.github.com>
2026-05-17 18:48:49 -0700

    Update AdminController.java

--- E_Commerce_App/src/main/java/in/spring/ecom/controller/AdminController.java
+++ E_Commerce_App/src/main/java/in/spring/ecom/controller/AdminController.java
@@ -19,7 +19,7 @@ public class AdminController
 {
 	@Autowired
 	private AdminService aService;
-	 @GetMapping("/dashboard")
+	 @GetMapping("/Dashboard")
 	    public String dashboard() {
 	        return "Dashboard";
 	    }
@@ -40,7 +40,7 @@ public String login(@RequestParam String email,
     if(admin != null)
     {
         session.setAttribute("loggedInAdmin", admin);  // add this
-        return "redirect:/dashboard";  // use redirect
+        return "redirect:/Dashboard";  // use redirect
     }
     else
     {
