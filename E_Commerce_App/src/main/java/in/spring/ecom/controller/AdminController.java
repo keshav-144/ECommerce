b71592539e1be25613cdadd8e3085d3e5625cd64
Keshav Kumar <167737709+keshav-144@users.noreply.github.com>
2026-05-17 19:30:50 -0700

    Update AdminController.java

--- E_Commerce_App/src/main/java/in/spring/ecom/controller/AdminController.java
+++ E_Commerce_App/src/main/java/in/spring/ecom/controller/AdminController.java
@@ -1,5 +1,4 @@
 package in.spring.ecom.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,66 +6,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import in.spring.ecom.entities.Admin;
-import in.spring.ecom.services.AdminService;
+import in.sp.main.entities.Admin;
+import in.sp.main.services.AdminService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AdminController 
 {
-    @Autowired
-    private AdminService aService;
-
-    // Open Dashboard Page
-    @GetMapping("/dashboard")
-    public String dashboard(HttpSession session)
-    {
-        Admin admin = (Admin) session.getAttribute("loggedInAdmin");
-        return "Dashboard";
-    }
-
-    // Open Login Page
+	@Autowired
+	private AdminService aService;
+	
+	 @GetMapping("/dashboard")
+	    public String dashboard() {
+	        return "Dashboard";
+	    }
+	
+	// Open adminLogin page
     @GetMapping("/admin/login")
-    public String showLoginPage()
-    {
+    public String showLoginPage() {
         return "admin";
     }
-
-    // Handle Login
+    
     @PostMapping("/admin/login")
-    public String login(@RequestParam String email,
-                        @RequestParam String password,
-                        Model model,
-                        HttpSession session)
+    public String login(@RequestParam String email,String password,Model model)
     {
-        Admin admin = aService.login(email, password);
-
-        if(admin != null)
-        {
-            session.setAttribute("loggedInAdmin", admin);
-
-            return "redirect:/dashboard";
-        }
-        else
-        {
-            model.addAttribute("error", "Invalid Credentials");
-
-            return "admin";
-        }
+    	Admin admin = aService.login(email, password);
+    	if(admin !=null)
+    	{
+    		return "Dashboard";
+    	}
+    	else
+    	{
+    		model.addAttribute("error","Admin not found");
+    		return "Admin";
+    	}
     }
-
-    // Logout
     @GetMapping("/admin/logout")
-    public String logout(HttpServletRequest request)
-    {
-        HttpSession session = request.getSession(false);
-
-        if(session != null)
-        {
-            session.invalidate();
-        }
-
-        return "redirect:/admin/login";
-    }
+	public String logout(HttpServletRequest request)
+	{
+		HttpSession session = request.getSession(false);
+		
+		if(session != null)
+		{
+			session.invalidate();
+		}
+		return "redirect:/admin/login";
+	}
 }
+
