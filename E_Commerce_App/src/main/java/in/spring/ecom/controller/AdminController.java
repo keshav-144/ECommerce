3154e09a97169fbb876e7c0fc7391dd159a356ef
Keshav Kumar <167737709+keshav-144@users.noreply.github.com>
2026-05-17 19:18:35 -0700

    Update AdminController.java

--- E_Commerce_App/src/main/java/in/spring/ecom/controller/AdminController.java
+++ E_Commerce_App/src/main/java/in/spring/ecom/controller/AdminController.java
@@ -1,20 +1,6 @@
 package in.spring.ecom.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import in.spring.ecom.entities.Admin;
-import in.spring.ecom.services.AdminService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;package in.spring.ecom.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -89,51 +75,4 @@ public class AdminController
 
         return "redirect:/admin/login";
     }
-}
-
-@Controller
-public class AdminController 
-{
-	@Autowired
-	private AdminService aService;
-	 @GetMapping("/Dashboard")
-	    public String dashboard() {
-	        return "Dashboard";
-	    }
-	
-	// Open adminLogin page
-    @GetMapping("/admin/login")
-    public String showLoginPage() {
-        return "admin";
-    }
-    
-   @PostMapping("/Dashboard")
-public String login(@RequestParam String email, 
-                    @RequestParam String password, 
-                    Model model,
-                    HttpSession session)  // add this
-{
-    Admin admin = aService.login(email, password);
-    if(admin != null)
-    {
-        session.setAttribute("loggedInAdmin", admin);  // add this
-        return "Dashboard";  // use redirect
-    }
-    else
-    {
-        model.addAttribute("error", "Admin not found");
-        return "redirect:/admin/login";  // lowercase to match your template
-    }
-}
-    @GetMapping("/admin/logout")
-	public String logout(HttpServletRequest request)
-	{
-		HttpSession session = request.getSession(false);
-		
-		if(session != null)
-		{
-			session.invalidate();
-		}
-		return "redirect:/admin/login";
-	}
 }
